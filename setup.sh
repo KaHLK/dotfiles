3b4e94a721ade5f6c5dc68952aa88c8f4dbf065c
KaHLK <krka@itu.dk>
2017-09-02 15:48:04 +0200

    Setup: Symlink all vim indent files and all fish function files

--- setup.sh
+++ setup.sh
@@ -5,11 +5,13 @@ mkdir -p .config/fish/functions
 ln -svf dotfiles/.vimrc .vimrc
 
 cd .vim/colors
-ln -svf ../../dotfiles/.vim/colors/molokai.vim molokai.vim
+ln -svf ../../dotfiles/.vim/colors/* ./
+
+cd ../indent
+ln -svf ../../dotfiles/.vim/indent/* ./
 
 cd ../../.config/fish
 ln -svf ../../dotfiles/fish/config.fish config.fish
 
 cd functions
-ln -svf ../../../dotfiles/fish/functions/fish_prompt.fish fish_prompt.fish
-ln -svf ../../../dotfiles/fish/functions/fish_right_prompt.fish fish_right_prompt.fish
+ln -svf ../../../dotfiles/fish/functions/* ./
