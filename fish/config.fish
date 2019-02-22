c6a882240505998bf402dc649a0e67e857de70ce
Kristoffer Kabat <kahlk@limelight-music.com>
2019-02-22 12:11:05 +0100

    Fish: Setup thefuck and start/or re-use already started git-agents

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,26 @@
 set -g -x fish_greeting ""
-cd ~
+# cd ~
+
+thefuck --alias | source
+
+# Check if an existing ssh-agent exists and re-use it
+set GOT_AGENT 0
+for FILE in /tmp/ssh-*/agent.*;
+ set SOCK_PID (string split "." $FILE)[2]
+ set PID (ps -fu$LOGNAME|awk '/ssh-agent/ && ( $2=='$SOCK_PID' || $3=='$SOCK_PID' || $2=='$SOCK_PID' +1 ) {print $2}')
+ set SOCK_FILE $FILE
+
+ set -x SSH_AUTH_SOCK $SOCK_FILE
+ set -x SSH_AGENT_PID $PID
+
+ ssh-add -l > /dev/null
+ if [ $status != 2 ]
+   set GOT_AGENT 1
+   echo "Found existing agent pid $PID"
+   break
+ end
+end
+
+if [ $GOT_AGENT = 0 ]
+ eval (ssh-agent -c)
+end
