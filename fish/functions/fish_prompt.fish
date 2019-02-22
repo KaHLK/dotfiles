d106fa72456cc2f64aa61a02ee02f23f5471525a
Kristoffer Kabat <kahlk@limelight-music.com>
2019-02-22 12:11:53 +0100

    Fish: Modify prompts (right now displays some git information)

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,15 @@
-function fish_prompt --description 'Write out the prompt'
-  printf '%s%s%s> ' (set_color 00afff) (prompt_pwd) (set_color normal)
+#function fish_prompt --description 'Write out the prompt'
+#  printf '%s%s%s> ' (set_color 00afff) (prompt_pwd) (set_color normal)
+#end
+
+function fish_prompt
+  set -l last_status $status
+  
+  printf '%s%s%s' (set_color 00afff) (prompt_pwd) (set_color normal)
+
+  if not test $last_status -eq 0
+    set_color $fish_color_error
+  end
+  echo -n '> '
+  set_color normal
 end
--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,3 +1,27 @@
 function fish_right_prompt --description "Write the right prompt"
-  printf "%s%s%s@%s%s" (set_color ff005f) (whoami) (set_color fafafa) (set_color 999900) (hostname|cut -d . -f 1)
+
+  if not set -q __fish_git_prompt_show_informative_status
+    set -g __fish_git_prompt_show_informative_status 1
+  end
+  if not set -q __fish_git_prompt_color_branch
+    set -g __fish_git_prompt_color_branch brmagenta
+  end
+  if not set -q __fish_git_prompt_showupstream
+    set -g __fish_git_prompt_showupstream "informative"
+  end
+  if not set -q __fish_git_prompt_showdirtystate
+    set -g __fish_git_prompt_showdirtystate "yes"
+  end
+  if not set -q __fish_git_prompt_color_stagedstate
+    set -g __fish_git_prompt_color_stagedstate yellow
+  end
+  if not set -q __fish_git_prompt_color_invalidstate
+    set -g __fish_git_prompt_color_invalidstate red
+  end
+  if not set -q __fish_git_prompt_color_cleanstate
+    set -g __fish_git_prompt_color_cleanstate brgreen
+  end
+  
+  printf "%s " (__fish_git_prompt)
+  printf "[%s%s%s@%s%s%s]" (set_color ff005f) (whoami) (set_color fafafa) (set_color 999900) (hostname|cut -d . -f 1) (set_color normal)
 end
