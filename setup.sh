6a9736794bd089509ca43cbe4b970c5fc4a55a63
Kristoffer Kabat <kahlk@limelight-music.com>
2019-04-15 14:44:37 +0200

    Added files for htop, xfce4-terminal, & .Xmodmap

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #/bin/bash
+echo "Looking for commands"
+# TODO: Check for all "required" programs. Missing: htop, xfce4-terminal
+if ! command -v nitrogen || ! command -v rofi
+then
+  echo "Please install 'nitrogen', 'rofi'"
+  exit
+fi
 if [ -z "$(ls -A Vundle.vim)" ]
 then
   set -x #Print commands
@@ -23,5 +30,19 @@ ln -svf ~/dotfiles/fish/config.fish config.fish
 
 cd ~/.config/fish/functions
 ln -svf ~/dotfiles/fish/functions/* ./
+
+cd ~/.config/xfce4/terminal/
+ln -svf ~/dotfiles/xfce4-terminal/* ./
+
+cd ~/.config/htop/
+ln -svf ~/dotfiles/htop/* ./
+
+cd ~
+ln -svf ~/dotfiles/.Xmodmap ./.Xmodmap
+
+# TODO: Maybe move all .config related files into a .config sub-dir
+# TODO: Split .i3/config and merge into .i3/config
+
+# TODO: Add bg and set it up with nitrogen
 
 vim +PluginInstall
