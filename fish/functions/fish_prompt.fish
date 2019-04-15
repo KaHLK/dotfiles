482a708c6ab744ae196f5d456f60261e36b1fed2
Kristoffer Kabat <kahlk@limelight-music.com>
2019-04-15 09:38:18 +0200

    Update fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -3,13 +3,8 @@
 #end
 
 function fish_prompt
-  set -l last_status $status
-  
-  printf '%s%s%s' (set_color 00afff) (prompt_pwd) (set_color normal)
+  printf '%s%s%s' (set_color ffb3ba) (prompt_pwd) (set_color normal)
 
-  if not test $last_status -eq 0
-    set_color $fish_color_error
-  end
   echo -n '> '
   set_color normal
 end
--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -23,5 +23,5 @@ function fish_right_prompt --description "Write the right prompt"
   end
   
   printf "%s " (__fish_git_prompt)
-  printf "[%s%s%s@%s%s%s]" (set_color ff005f) (whoami) (set_color fafafa) (set_color 999900) (hostname|cut -d . -f 1) (set_color normal)
+  printf "[%s%s%s@%s%s%s]" (set_color ffb3ba) (whoami) (set_color normal) (set_color bae1ff) (hostname|cut -d . -f 1) (set_color normal)
 end
