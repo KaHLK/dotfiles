2bf9caf0a5ef4931af0531ab6272a1366dce72b9
Kristoffer Kabat <yeah-kris@hotmail.com>
2026-02-17 11:20:39 +0100

    docs: add comment explaining cd in init/uninit scripts

--- uninit.sh
+++ uninit.sh
@@ -4,6 +4,8 @@ set -e
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo "Unstowing dotfiles from $DOTFILES_DIR..."
+# stow uses the current directory as the stow dir by default,
+# so we must cd here regardless of where the script was called from
 cd "$DOTFILES_DIR"
 
 stow -D bash fish git
