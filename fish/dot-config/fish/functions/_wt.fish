a6fd37e5fbae3a5d862c44b56212b25c3773dc1a
Kristoffer Kabat <yeah-kris@hotmail.com>
2026-02-20 12:40:37 +0100

    Fix split-window targeting wrong window in _wt.fish

--- fish/dot-config/fish/functions/_wt.fish
+++ fish/dot-config/fish/functions/_wt.fish
@@ -6,6 +6,6 @@ function _wt --description "Open directory in opencode"
             split-window -h -c $dir -l 33%
     else
         tmux new-window -c $dir "opencode"
-        tmux split-window -h -t :\$ -c $dir -l 33%
+        tmux split-window -h -c $dir -l 33%
     end
 end
