dbde9046f1b08dad996aba43f1a919855f73638e
Kristoffer Kabat <yeah-kris@hotmail.com>
2026-03-10 00:14:16 +0100

    Align dotfiles with omarchy installation

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -57,16 +57,16 @@ if status is-interactive
     end
 end
 
-set __fish_git_prompt_showuntrackedfiles yes
-set __fish_git_prompt_showdirtystate yes
-set __fish_git_prompt_showstashstate ''
-set -g fish_prompt_pwd_dir_length 3
-set __fish_git_prompt_show_informative_status 1
-set __fish_git_prompt_color_branch brmagenta
-set __fish_git_prompt_showupstream informative
-set __fish_git_prompt_color_stagedstate yellow
-set __fish_git_prompt_color_invalidstate red
-set __fish_git_prompt_color_cleanstate brgreen
+# set __fish_git_prompt_showuntrackedfiles yes
+# set __fish_git_prompt_showdirtystate yes
+# set __fish_git_prompt_showstashstate ''
+# set -g fish_prompt_pwd_dir_length 3
+# set __fish_git_prompt_show_informative_status 1
+# set __fish_git_prompt_color_branch brmagenta
+# set __fish_git_prompt_showupstream informative
+# set __fish_git_prompt_color_stagedstate yellow
+# set __fish_git_prompt_color_invalidstate red
+# set __fish_git_prompt_color_cleanstate brgreen
 
 set host $hostname
 switch (uname)
@@ -74,53 +74,53 @@ switch (uname)
         set host (scutil --get ComputerName)
 end
 
-function fish_prompt
-    set -l exitcode "$status"
-
-    # print the "show cursor" escape code in case a program exited while the cursor was hidden
-    printf "\033[?25h"
-
-    printf (set_color reset)(set_color grey)
-    printf "\n\033[1F"
-
-    # if is a git repository
-    if git rev-parse --is-inside-work-tree >/dev/null 2>/dev/null
-        # repo name
-        set -l reporoot (git rev-parse --show-toplevel 2>/dev/null)
-        printf (set_color blue)(basename $reporoot)
-
-        # branch
-        set -l branchname (git symbolic-ref --short HEAD 2>/dev/null)
-        set -l mainbranch (git symbolic-ref refs/remotes/origin/HEAD 2>/dev/null)
-        set -l mainbranch (string sub -s 11 "$mainbranch")
-        if test -z "$mainbranch"
-            set mainbranch main
-        end
-        if test "$branchname" != "$mainbranch"
-            printf (set_color magenta)(string trim (fish_git_prompt))
-        end
-
-        # rest of the path
-        printf (set_color grey)(string join "/" (string split "/" (string sub -s (math (string length $reporoot) + 1) $PWD)))
-    else
-        # non git path formatting
-        switch "$PWD"
-            case "$HOME*"
-                printf "~"(string sub -s (math (string length $HOME) + 1) "$PWD")
-            case "/tmp/scratchpad_*"
-                printf (set_color brred)"scratch"(set_color grey)(string sub -s 32 "$PWD")
-            case '*'
-                printf "%s" "$PWD"
-        end
-    end
-
-    # print the $
-    if test "$exitcode" -ne 0
-        printf (set_color brred)" >"(set_color brwhite)" "
-    else
-        printf (set_color brwhite)" > "
-    end
-end
+# function fish_prompt
+#     set -l exitcode "$status"
+#
+#     # print the "show cursor" escape code in case a program exited while the cursor was hidden
+#     printf "\033[?25h"
+#
+#     printf (set_color reset)(set_color grey)
+#     printf "\n\033[1F"
+#
+#     # if is a git repository
+#     if git rev-parse --is-inside-work-tree >/dev/null 2>/dev/null
+#         # repo name
+#         set -l reporoot (git rev-parse --show-toplevel 2>/dev/null)
+#         printf (set_color blue)(basename $reporoot)
+#
+#         # branch
+#         set -l branchname (git symbolic-ref --short HEAD 2>/dev/null)
+#         set -l mainbranch (git symbolic-ref refs/remotes/origin/HEAD 2>/dev/null)
+#         set -l mainbranch (string sub -s 11 "$mainbranch")
+#         if test -z "$mainbranch"
+#             set mainbranch main
+#         end
+#         if test "$branchname" != "$mainbranch"
+#             printf (set_color magenta)(string trim (fish_git_prompt))
+#         end
+#
+#         # rest of the path
+#         printf (set_color grey)(string join "/" (string split "/" (string sub -s (math (string length $reporoot) + 1) $PWD)))
+#     else
+#         # non git path formatting
+#         switch "$PWD"
+#             case "$HOME*"
+#                 printf "~"(string sub -s (math (string length $HOME) + 1) "$PWD")
+#             case "/tmp/scratchpad_*"
+#                 printf (set_color brred)"scratch"(set_color grey)(string sub -s 32 "$PWD")
+#             case '*'
+#                 printf "%s" "$PWD"
+#         end
+#     end
+#
+#     # print the $
+#     if test "$exitcode" -ne 0
+#         printf (set_color brred)" >"(set_color brwhite)" "
+#     else
+#         printf (set_color brwhite)" > "
+#     end
+# end
 
 function fish_greeting
     switch (uname)
@@ -157,4 +157,6 @@ zoxide init fish | source
 fnm env --use-on-cd | source
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/kahlk/.google/path.fish.inc' ]; . '/Users/kahlk/.google/path.fish.inc'; end
+if [ -f '/Users/kahlk/.google/path.fish.inc' ]
+    . '/Users/kahlk/.google/path.fish.inc'
+end
