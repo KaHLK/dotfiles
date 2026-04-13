6b2f020f0cc7b949887ecd87f54295e6d54ed8de
Kristoffer Kabat <yeah-kris@hotmail.com>
2026-04-13 09:49:29 +0200

    fish: _wt launch claude instead of opencode

--- fish/dot-config/fish/functions/_wt.fish
+++ fish/dot-config/fish/functions/_wt.fish
@@ -2,10 +2,10 @@ function _wt --description "Open directory in opencode"
     set -l dir $argv[1]
 
     if not set -q TMUX
-        tmux new-session -c $dir "opencode" \; \
+        tmux new-session -c $dir "claude --verbose" \; \
             split-window -h -c $dir
     else
-        tmux new-window -c $dir "opencode"
+        tmux new-window -c $dir "claude --verbose"
         tmux split-window -h -c $dir
     end
 end
